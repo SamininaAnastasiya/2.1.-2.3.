f6c9807a1df52a7b2a239be4524286f8713d24f5
Nastya <N@n.ru>
2022-12-04 20:54:51 +0300

    first commit04/12

--- task_2/solution.js
+++ task_2/solution.js
@@ -2,7 +2,16 @@ function calcShipping(sum, min, shipping) {
     let productsSum = sum; // сумма в корзине
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
-
+    let shippingSum;
+    if (productsSum==0) {
+        shippingSum=0
+    }
+    if (productsSum>=freeShippingMinSum) {
+        shippingSum=0
+    }
+    if (productsSum>0 && productsSum<freeShippingMinSum) {
+        shippingSum=shippingPrice
+    }
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
@@ -25,7 +34,12 @@ function calcDiscount(sum, min, discount) {
     let productsSum = sum; // сумма в корзине
     let discountMinSum = min; // минимальная цена для скидки
     let discountPart = discount; // величина скидки в процентах
-
+    let discountSum;
+    if (productsSum>=discountMinSum) {
+        discountSum=productsSum*discountPart/100
+    } else
+    {discountSum=0
+    }
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
@@ -42,6 +56,9 @@ function calcDiscount(sum, min, discount) {
 function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shippingPrice}) {
     let productsSum = sum;
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
+    let totalSum;
+    totalSum=productsSum;
+    totalSum=totalSum-discountSum;
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
 
@@ -51,8 +68,11 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
-
+    totalSum=totalSum+shippingSum;
     // прибавьте к totalSum значение shippingSum
+    
+    let freeShipping;
+    (shippingSum == 0) ? freeShipping = true : freeShipping = false;
 
     // создайте переменную freeShipping
     // запишите без использования if или любых других условий:
